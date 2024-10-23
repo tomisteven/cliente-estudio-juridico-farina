02408e5d72675c7906d0e459a59a0e5e499f268b
Tomas Steven Desktop <tomssteven22@gmail.com>
2024-10-23 16:32:20 -0300

    btn whatsapp

--- src/components/buttonWhatsApp/ButtonWhatsApp.js
+++ src/components/buttonWhatsApp/ButtonWhatsApp.js
@@ -4,7 +4,7 @@ import './ButtonWhatsApp.css'
 export default function ButtonWhatsApp() {
   return (
     <div className='cont-button-whats'>
-        <a href="https://wa.me/5492214281440?text=Hola%20Dr.%20Farina%20Balbi%20necesito%20hacer%20una%20consulta" target="_blank" rel='noreferrer'>
+        <a href="https://wa.link/8mjb14" target="_blank" rel='noreferrer'>
             <div className="whatsapp">
             <i className="fab fa-whatsapp"></i>
             </div>
